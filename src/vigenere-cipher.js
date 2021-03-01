12db1f62377bc4bdefe8e03ae66903c5558a163f
iGrek <59765393+iGrekInLapland@users.noreply.github.com>
2021-03-01 22:36:38 +0300

    feat: add solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,57 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(directly){
+    this.directly = directly
   }
-}
+  encrypt(m, k) {
+    
+   // if(!!m || !!k) throw new Error('something is wrong');
+    let a = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let res = [];
+    m = m.toUpperCase();
+    k = k.toUpperCase();
+    let j = 0; 
+    for(let i = 0; i < m.length; i++){
+     
+      let s;
+      if( a.indexOf(m[i]) === -1){
+        res.push(m[i]);
+        j--
+      }else{
+        s = a.indexOf(m[i]) + a.indexOf(k[((j < k.length) ? j : j%k.length )]);
+      res.push(a[((s<a.length) ? s : s%a.length )])
+      }   
+      j++
+    }
+    if(this.directly === true ||this.directly === undefined) return res = res.join(''); 
+    return res.reverse().join('')
+  }    
+  decrypt(m, k){
+   // if(!!m || !!k) throw new Error("something is wrong");
+      let a = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      let res = [];
+      m = m.toUpperCase();
+      k = k.toUpperCase();
+      let j = 0; 
+      for(let i = 0; i < m.length; i++){
+       
+        let s;
+        if( a.indexOf(m[i]) === -1){
+          res.push(m[i]);
+          j--
+        }else{
+          s = a.indexOf(m[i]) - a.indexOf(k[((j < k.length) ? j : j%k.length )]);
+          (s < 0)? s = s + a.length : s;
+        res.push(a[((s<a.length) ? s : s%a.length )])
+        }   
+        j++
+      }
+      if(this.directly === true ||this.directly === undefined) return res = res.join(''); 
+    return res.reverse().join('')
+    }
+  }
+
 
 module.exports = VigenereCipheringMachine;
+
