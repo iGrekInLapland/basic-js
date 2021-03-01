9fb1eb921ab6acf181d58a78e8ca6f0494d100d0
iGrek <59765393+iGrekInLapland@users.noreply.github.com>
2021-03-01 17:03:44 +0300

    feat: add solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,39 @@
-const CustomError = require("../extensions/custom-error");
+//const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports =    function repeater(str, ob) {
+  let res, a, b;
+  b = [];
+  a = [];
+ 
   
+  if (typeof str !== "string") str = String(str);
+  if (!ob.repeatTimes) ob.repeatTimes = 1;
+  if (!ob.separator ) ob.separator = "+";
+  if (!ob.additionRepeatTimes ) ob.additionRepeatTimes = 1;
+  if (!ob.additionSeparator) ob.additionSeparator = "|";
+  
+  
+  if(ob.addition !== undefined){
+    if(typeof ob.addition !== "string") ob.addition = String(ob.addition);
+    
+    for (let i = 0; i < ob.additionRepeatTimes; i++) {
+      a[i] = ob.addition;
+    }
+    
+    if(a.length > 1) {
+      a = a.join(ob.additionSeparator);
+    }else{ 
+      a = a.join('')
+    }
+   
+    res = str + a;
+  }else{
+    res = str;
+  }
+  
+  for (let i = 0; i< ob.repeatTimes; i++) {
+    b[i] = res;
+  }
+  return b = b.join(ob.separator)
+    
+  }
