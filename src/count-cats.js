7858791ff8e3203c3f4a5bd0d5e00bb6b61fe642
iGrek <59765393+iGrekInLapland@users.noreply.github.com>
2021-03-02 22:32:09 +0300

    feat: add resolution

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,12 +1,12 @@
 //const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(matrix) {
- // throw new CustomError('Not implemented');
+module.exports = function countCats(mat) {
+ 
   let counter = 0;
-  for( let i = 0; i < matrix.length; i++){
-    for (let j = 0; j < matrix[i].length; j++) {
-      if(matrix[i][j] === "^^") counter++
-    }
-  }
+  mat = [].concat(...mat).forEach(element => {
+      if(element==='^^') counter++
+    });
+  
+  
   return counter;
 };
