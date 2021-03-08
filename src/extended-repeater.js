0535b21c21a757b1c8ac3286be4701cee7e5527e
iGrek <59765393+iGrekInLapland@users.noreply.github.com>
2021-03-08 17:03:54 +0300

    feat: add new solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,39 +1,16 @@
-//const CustomError = require("../extensions/custom-error");
+   const CustomError = require('../extensions/custom-error');
 
-module.exports =    function repeater(str, ob) {
-  let res, a, b;
-  b = [];
-  a = [];
- 
-  
-  if (typeof str !== "string") str = String(str);
-  if (!ob.repeatTimes) ob.repeatTimes = 1;
-  if (!ob.separator ) ob.separator = "+";
-  if (!ob.additionRepeatTimes ) ob.additionRepeatTimes = 1;
-  if (!ob.additionSeparator) ob.additionSeparator = "|";
-  
-  
-  if(ob.addition !== undefined){
-    if(typeof ob.addition !== "string") ob.addition = String(ob.addition);
-    
-    for (let i = 0; i < ob.additionRepeatTimes; i++) {
-      a[i] = ob.addition;
-    }
-    
-    if(a.length > 1) {
-      a = a.join(ob.additionSeparator);
-    }else{ 
-      a = a.join('')
-    }
-   
-    res = str + a;
-  }else{
-    res = str;
+module.exports = function repeater(str, options) {
+  let res = [];
+  let add = [];
+
+  while (add.length < (options.additionRepeatTimes || 1)) {
+    add.push(options.addition || options.addition === null || options.addition === false ? String(options.addition) : '' );
   }
-  
-  for (let i = 0; i< ob.repeatTimes; i++) {
-    b[i] = res;
+
+  while (res.length < (options.repeatTimes || 1)) {
+    res.push(str + add.join(options.additionSeparator || '|'));
   }
-  return b = b.join(ob.separator)
-    
-  }
+
+  return res.join(options.separator || '+');
+};
